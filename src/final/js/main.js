ae767e22c9e4775a32efcb53b1420537a20a2498
drussell1974 <dave@jazzthecat.co.uk>
2021-08-04 13:55:36 +0100

    show hide code

--- src/final/js/main.js
+++ src/final/js/main.js
@@ -1,3 +1,22 @@
-function showCode(id) {
-
+function showCode(parentElement) {
+	var parentElementDiv = document.getElementById(parentElement);
+	var selector = '#' + parentElement + ' .example';
+	console.log(selector);
+	var exampleElement = document.querySelector(selector);
+	if (parentElementDiv.className === 'section open') {
+		parentElementDiv.className = 'section';
+		var codeElement = document.querySelector(selector + ' .example-code');
+		exampleElement.removeChild(codeElement);
+		
+	} else {
+		var text = exampleElement.innerHTML;
+		var code = document.createTextNode(text);
+		var codeElement = document.createElement('div');
+		codeElement.appendChild(code);
+		codeElement.className = 'example-code';
+		exampleElement.appendChild(codeElement);
+		document.querySelector('#' + parentElement + ' .code-show-button').value = 'Hide code';
+			
+		parentElementDiv.classList.add('open');	
+	}
 }
